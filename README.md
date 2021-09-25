9d1a4e9e4af490274767f1ba5a271c9d5b55c489
Jerry Chang <therealjerrycan@gmail.com>
2021-09-25 14:58:57 -0700

    init repo with changes

---
+++ README.md
@@ -0,0 +1,10 @@
+## LightBnB (fork)
+
+This is a fork of [LHL LightBnB_WebApp](https://github.com/lighthouse-labs/LightBnB_WebApp).
+
+#### Changes:
+
+- Integrate [@common-web/esbuild@1.0.19](https://www.npmjs.com/package/@common-web/esbuild) for building and bundling
+- Integrate [@common-web/ts-config@1.0.1](https://www.npmjs.com/package/@common-web/ts-config) for typescript 
+- Moved `entry` from `index.js` to `index.ts`
+- Updated the local scripts `build`, `start`, `start:watch`
---
+++ build.js
@@ -0,0 +1,20 @@
+const config = require('@common-web/esbuild');
+const esbuild = require('esbuild');
+const path = require('path');
+const copyFiles = require('./scripts/copy-files');
+const packageJson = require('./package.json');
+
+// Simple
+esbuild.build(config.getBaseConfig({
+    entryPoint: './src/server/index.ts',
+    platform: 'node',
+    bundle: true,
+    external: [
+        ...Object.keys(packageJson.dependencies)
+    ]
+}))
+.then(copyFiles('./src/public', './dist/public', { recursive: true }))
+.then(() => {
+    console.log('Build finished');
+});
+

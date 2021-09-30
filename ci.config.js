24bcf5f015a87f167844128faace4fed1f47d016
Jerry Chang <therealjerrycan@gmail.com>
2021-09-29 22:09:05 -0700

    Add parse boolean

--- ci.config.js
+++ ci.config.js
@@ -1,10 +1,16 @@
 const get = require('lodash.get');
 
+function parseBool(value) {
+  if (value === 'true' || value === 'True') return true;
+  if (value === 'false' || value === 'false') return false;
+  return false;
+}
+
 const getConfigurations = () => {
-  const debug = process.env.CI_DEBUG;
+  const debug = parseBool(process.env.CI_DEBUG);
   const nodeEnv = process.env.NODE_ENV;
   const isProduction = nodeEnv === 'production';
-  const collectCoverage = get(process.env, 'CI_JEST_COVERAGE', false);
+  const collectCoverage = parseBool(get(process.env, 'CI_JEST_COVERAGE', false));
   const config = {
     nodeEnv,
     isProduction,
